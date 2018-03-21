3e5dd33793421956f14c00cb0a780b3937d51f55
juliagviu <juliagviu97@gmail.com>
2018-03-21 13:09:46 +0100

    Stats usuario VO

--- bbdd/Prueba.java
+++ bbdd/Prueba.java
@@ -4,29 +4,32 @@ import java.sql.SQLException;
 
 public class Prueba {
     public static void main(String[] args) throws PropertyVetoException, SQLException, IOException, ExceptionCampoInvalido {
-        UsuarioVO u = new UsuarioVO("juliagviu", "716185@unizar.es", "Julia", "Guerrero", true);
+        UsuarioVO u = new UsuarioVO("juliagv", "716185@unizar.es", "Julia", "Guerrero", true);
         InterfazDatos.instancia().crearUsuario(u);
-        UsuarioVO nuevo = InterfazDatos.instancia().obtenerDatosUsuario("juliagviu");
+        UsuarioVO nuevo = InterfazDatos.instancia().obtenerDatosUsuario("juliagv");
         nuevo.mostrar();
         nuevo.setCorreo("ahoraotrocorreo@unizar.es");
         nuevo.setApellidos("Viu");
         InterfazDatos.instancia().modificarDatosUsuario(nuevo);
-        nuevo = InterfazDatos.instancia().obtenerDatosUsuario("juliagviu");
+        nuevo = InterfazDatos.instancia().obtenerDatosUsuario("juliagv");
         nuevo.mostrar();
-
-        if (InterfazDatos.instancia().esAdministrador("juliagviu")){
-            System.out.println("TODO CONTROLADO!!");        
-        }
 
         nuevo.setAdmin(false);
         InterfazDatos.instancia().modificarDatosUsuario(nuevo);
-        nuevo = InterfazDatos.instancia().obtenerDatosUsuario("juliagviu");
+        nuevo = InterfazDatos.instancia().obtenerDatosUsuario("juliagv");
+    
+        InterfazDatos.instancia().eliminarUsuario("juliagv");
         
-        if(!InterfazDatos.instancia().esAdministrador("juliagviu")){
-            System.out.println("TODO SIGUE CONTROLADO!!");
+        StatsUsuarioVO su = new StatsUsuarioVO("juliagv");
+        
+        if(su.getPuntuacion()==-1){
+            System.out.println("TODO CONTROLADO!!"); 
+        }        
+
+        su.setPuntuacion(3);
+
+        if(su.getPuntuacion()==3){
+            System.out.println("TODO CONTROLADO!!"); 
         }
-        InterfazDatos.instancia().eliminarUsuario("juliagviu");
-        
-        
     }
 }
---
+++ bbdd/StatsUsuarioDAO.java
@@ -0,0 +1,20 @@
+/*
+ * Autor: Guerrero Viu, Julia
+ * Fecha: 20-03-2018
+ * Fichero: UsuarioDAO.java
+ */
+
+import java.text.SimpleDateFormat;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import java.beans.PropertyVetoException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+import java.io.FileInputStream;
+
+public class StatsUsuarioDAO {
+        
+}
