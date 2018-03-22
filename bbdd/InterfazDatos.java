9a8be1656b73de082937741152d2dd9aac0c420d
serizba <serizba@gmail.com>
2018-03-22 18:24:26 +0100

    Conflictos merge arreglados

--- bbdd/InterfazDatos.java
+++ bbdd/InterfazDatos.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Properties;
 import java.io.FileInputStream;
 
