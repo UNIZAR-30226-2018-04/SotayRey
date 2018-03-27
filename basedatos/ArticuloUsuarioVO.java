37861c472d6f85f489c4690284f3e061ab40f3f8
serizba <serizba@gmail.com>
2018-03-27 20:27:15 +0200

    Movido todo el sistema de archivos, los imports van bien

---
+++ basedatos/ArticuloUsuarioVO.java
@@ -0,0 +1,65 @@
+
+/*
+ * Autor: Sergio Izquierdo Barranco
+ * Fecha: 22-03-18
+ * Fichero: Objeto ArtículoUsuario
+ */
+
+
+public class ArticuloUsuarioVO extends ArticuloVO {
+    private boolean disponible;
+    private boolean favorito;
+    private boolean comprado;
+    private String username;
+
+    public ArticuloUsuarioVO(String nombre, int precio, String descripcion, String rutaImagen, char tipo, boolean disponible, boolean favorito, boolean comprado, String username) {
+        super(nombre, precio, descripcion, rutaImagen, tipo);
+        this.disponible = disponible;
+        this.comprado = comprado;
+        this.username = username;
+        this.favorito  = favorito;
+    }
+
+
+
+//    public ArticuloUsuarioVO(String nombre, int precio, String descripcion, String rutaImagen, char tipo, Liga requiere, boolean disponible, boolean favorito, boolean comprado, String username) {
+//        super(nombre, precio, descripcion, rutaImagen, tipo, requiere);
+//        this.disponible = disponible;
+//        this.comprado = comprado;
+//        this.username = username;
+//        this.favorito = favorito;
+//    }
+
+
+    public boolean isFavorito() {
+        return favorito;
+    }
+
+    public void setFavorito(boolean favorito) {
+        this.favorito = favorito;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public boolean isComprado() {
+        return comprado;
+    }
+
+    public void setComprado(boolean comprado) {
+        this.comprado = comprado;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
---
+++ basedatos/README.md
@@ -0,0 +1,8 @@
+Compilacion interfaz base de datos:
+'''
+javac -cp lib/*:. *.java
+'''
+Ejecucion interfaz base de datos:
+'''
+java -cp lib/*:. PruebaBases
+'''
