0a8e4317fc7c724416ddb0bcfe53ef96645e40ed
Victor Soria <victorspgl@gmail.com>
2018-04-01 20:26:03 +0200

    Test insertar usuario

--- src/servlet/RegistroServlet.java
+++ src/servlet/RegistroServlet.java
@@ -28,106 +28,89 @@ public class RegistroServlet extends HttpServlet {
         String password = request.getParameter("passwd");
         String passwordRep = request.getParameter("passwdRep");
         String error ="";
-        try{
-            //TODO: todos los == (equals) están mal
-            //Evitar campos NULL
-            if(login=="" || login ==null){
-                error= "emptyUser";
-                request.setAttribute("error",error);
-                // response.sendRedirect("index.jsp");
-                RequestDispatcher dispatcher=request.getRequestDispatcher("jsp/login.jsp");
-                dispatcher.forward(request,response);
-            }
-            else if(apellidos=="" || apellidos ==null){
-                error= "emptyLastName";
-                request.setAttribute("error",error);
-                RequestDispatcher dispatcher=request.getRequestDispatcher("jsp/login.jsp");
-                dispatcher.forward(request,response);
-            }
-            else if(nombre=="" || nombre ==null){
-                error= "emptyName";
-                request.setAttribute("error",error);
-                RequestDispatcher dispatcher=request.getRequestDispatcher("jsp/login.jsp");
-                dispatcher.forward(request,response);
-            }
-            else if(email=="" || email ==null){
-                error= "emptyEmail";
-                request.setAttribute("error",error);
-                RequestDispatcher dispatcher=request.getRequestDispatcher("jsp/login.jsp");
-                dispatcher.forward(request,response);
-            }
-            else if(password=="" || password ==null){
-                error= "emptyPass";
-                request.setAttribute("error",error);
-                RequestDispatcher dispatcher=request.getRequestDispatcher("jsp/login.jsp");
-                dispatcher.forward(request,response);
-            }
-            else if(passwordRep=="" || passwordRep ==null){
-                error= "emptyRePass";
-                request.setAttribute("error",error);
-                RequestDispatcher dispatcher=request.getRequestDispatcher("jsp/login.jsp");
-                dispatcher.forward(request,response);
-            }
-            else if(!password.equals(passwordRep)){
-                error= "wrongRePass";
-                request.setAttribute("error",error);
-                RequestDispatcher dispatcher=request.getRequestDispatcher("jsp/login.jsp");
-                dispatcher.forward(request,response);
-            }
-            else{//Si todos los datos no son null
-                UsuarioVO usuarioVo = null;
-                InterfazDatos facade = null;
+        //Evitar campos NULL
+        if(login == null || login.equals("")){
+            error= "emptyUser";
+            request.setAttribute("error",error);
+            // response.sendRedirect("index.jsp");
+            RequestDispatcher dispatcher=request.getRequestDispatcher("jsp/login.jsp");
+            dispatcher.forward(request,response);
+        }
+        else if(apellidos == null || apellidos.equals("")){
+            error= "emptyLastName";
+            request.setAttribute("error",error);
+            RequestDispatcher dispatcher=request.getRequestDispatcher("jsp/login.jsp");
+            dispatcher.forward(request,response);
+        }
+        else if(nombre == null || nombre.equals("")){
+            error= "emptyName";
+            request.setAttribute("error",error);
+            RequestDispatcher dispatcher=request.getRequestDispatcher("jsp/login.jsp");
+            dispatcher.forward(request,response);
+        }
+        else if(email == null || email.equals("")){
+            error= "emptyEmail";
+            request.setAttribute("error",error);
+            RequestDispatcher dispatcher=request.getRequestDispatcher("jsp/login.jsp");
+            dispatcher.forward(request,response);
+        }
+        else if(password == null || password.equals("")){
+            error= "emptyPass";
+            request.setAttribute("error",error);
+            RequestDispatcher dispatcher=request.getRequestDispatcher("jsp/login.jsp");
+            dispatcher.forward(request,response);
+        }
+        else if(passwordRep == null || passwordRep.equals("")){
+            error= "emptyRePass";
+            request.setAttribute("error",error);
+            RequestDispatcher dispatcher=request.getRequestDispatcher("jsp/login.jsp");
+            dispatcher.forward(request,response);
+        }
+        else if(!password.equals(passwordRep)){
+            error= "wrongRePass";
+            request.setAttribute("error",error);
+            RequestDispatcher dispatcher=request.getRequestDispatcher("jsp/login.jsp");
+            dispatcher.forward(request,response);
+        }
+        else{//Si todos los datos no son null
+            UsuarioVO usuarioVo = null;
+            InterfazDatos facade = null;
 
-                try {
-                    //TODO: Falta añadir la contraseña
-                    usuarioVo = new UsuarioVO(login, password, email, nombre, apellidos, false);
-                }
-                catch(Exception e){
-                    System.out.println(e.toString());
-                    //TODO: Tratar error
-                }
+            try {
+                usuarioVo = new UsuarioVO(login, password, email, nombre, apellidos, false);
+            }
+            catch(Exception e){
+                System.out.println(e.toString());
+                //TODO: Tratar error
+            }
 
-                try {
-                    facade = InterfazDatos.instancia();
-                }
-                catch(Exception e){
-                    System.out.println(e.toString());
-                    //TODO: Tratar error
-                }
+            try {
+                facade = InterfazDatos.instancia();
+            }
+            catch(Exception e){
+                System.out.println(e.toString());
+                //TODO: Tratar error
+            }
 
+            try {
                 facade.crearUsuario(usuarioVo);
-
-                HttpSession sesion= request.getSession();
-                sesion.setAttribute("userId", login);
-                sesion.setMaxInactiveInterval(24*60*60);
-
-
-                response.sendRedirect("home.jsp");
+            } catch (Exception e){
+                error= "existentUser";
+                request.setAttribute("error",error);
+                RequestDispatcher dispatcher=request.getRequestDispatcher("jsp/login.jsp");
+                dispatcher.forward(request,response);
             }
 
-        } catch(Exception e){
-            System.out.println(e);
+            HttpSession sesion= request.getSession();
+            sesion.setAttribute("userId", login);
+            sesion.setMaxInactiveInterval(24*60*60);
 
-            if((e.toString()).contains("PRIMARY")){
-                //Registro incorrecto, email esta en uso
-                error = "El e-mail ya esta en uso.";
-            }
-            else if((e.toString()).contains("login")){
-                error= "El usuario ya esta en uso.";
-            }
-            else if((e.toString()).contains("date")){
-                error= "Introduce una fecha de nacimiento.";
-            }
-            else{
-                error= "No se ha podido completar el registro. Intentalo de nuevo.";
-            }
-            request.setAttribute("errors",error);
 
+            response.sendRedirect("jsp/home.jsp");
         }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
         doPost(request,response);
     }
-
 }
