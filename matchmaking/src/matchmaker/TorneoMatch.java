7a637d1e3b8fdc1edeae55f0a6bc6a1ce352f7c6
0nly <corbalrob@gmail.com>
2018-05-23 12:37:34 +0200

    Inicio de implementación de torneos.

---
+++ matchmaking/src/matchmaker/TorneoMatch.java
@@ -0,0 +1,65 @@
+package matchmaker;
+
+import basedatos.modelo.TorneoVO;
+
+import javax.websocket.Session;
+import java.math.BigInteger;
+import java.util.HashMap;
+
+/**
+ * Autor: Javier
+ * Fecha: 21/05/2018
+ */
+public class TorneoMatch {
+    private HashMap<String, Session> jugadores;
+    private TorneoVO vo;
+    private int fase;
+    private boolean lleno;
+
+    public TorneoMatch(TorneoVO t) {
+        this.jugadores = new HashMap<>();
+        this.vo = t;
+        this.lleno = false;
+        fase = 0;
+    }
+
+    public void setVO(TorneoVO t) {
+        this.vo = t;
+    }
+
+    public TorneoVO getVO() {
+        return this.vo;
+    }
+
+    public void anyadirJugador(String nombre, Session sesion) {
+        if (!jugadores.containsKey(nombre)) {
+            jugadores.put(nombre, sesion);
+        }
+    }
+
+    public void eliminarJugador(String nombre) {
+        if (jugadores.containsKey(nombre)) {
+            jugadores.remove(nombre);
+        }
+    }
+
+    public int getFase() {
+        return this.fase;
+    }
+
+    public void incFase() {
+        this.fase++;
+    }
+
+    public BigInteger getIdTorneo() {
+        return this.vo.getId();
+    }
+
+    public Session getSesion(String nombre) {
+        return jugadores.get(nombre);
+    }
+
+    public void setLleno(boolean valor) {
+        this.lleno = valor;
+    }
+}
